45e482cf5b78afcd131319989723f4e651dfe459
Chris Hailey <chris.hailey8@gmail.com>
2018-11-05 02:22:09 -0800

    casdf

--- app/index.js
+++ app/index.js
@@ -1,13 +1,6 @@
 
 var express = require('express');
-
 var router = express.Router();
-
-
-
-
-//const results = Papa.parse("metro-bike-share-trip-data.csv");
-//console.log(results[0]);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
